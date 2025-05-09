cda05ce5cf2598e588cd084676112de6b76edb05
jmo5896 <justin.m.moore5896@gmail.com>
2025-05-09 12:37:04 -0500

    starts dialing in the space scene, stars are twinkling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Canvas } from "@react-three/fiber"
+import { Stars } from "@react-three/drei"
 
 export default function App() {
 
   return (
-    <>
-      <h1>HELLO PORTFOLIO!!!!!</h1>
-    </>
+    <div id="canvas-container">
+      <Canvas>
+        <color
+          attach='background'
+          args={["black"]}
+        />
+        <Stars />
+        {/* <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh> */}
+      </Canvas>
+    </div>
   )
 }
