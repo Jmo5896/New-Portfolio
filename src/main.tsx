cd264d00ee722442686a517cdf15145cdecdffdc
jmo5896 <justin.m.moore5896@gmail.com>
2025-05-09 11:57:43 -0500

    adds routing to main file and adds all the pages I'll be using for this project

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,47 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css'
 
 import App from './App.tsx'
+import AboutMe from './pages/AboutMe.tsx'
+import Contact from './pages/Contact.tsx'
+import Error from './pages/Error.tsx'
+import Landing from './pages/Landing.tsx'
+import Projects from './pages/Projects.tsx'
+import Resume from './pages/Resume.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: '/aboutme',
+        element: <AboutMe />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/projects',
+        element: <Projects />
+      },
+      {
+        path: 'resume',
+        element: <Resume />
+      }
+    ]
+  }
+])
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
